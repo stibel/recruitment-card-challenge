d0bbc41f084cf490089e8a6c40b1ad4c3bfabaa5
Mikołaj Siebielec <mikisiebielec@gmail.com>
2024-09-26 11:57:23 +0200

    refactor(misc): refactor html template, and scss and js code

--- js/modules/custom.js
+++ js/modules/custom.js
@@ -1,65 +1,86 @@
 const defaultCardNumber = '#### #### #### ####';
 const defaultCardHolder = 'JAN KOWALSKI';
 
-export default function(){
-    const cardNumberInput = document.querySelector('.card__form--input_card_number');
-    const cardHolderInput = document.querySelector('.card__form--input_card_name');
-    const cardNumberPreview = document.querySelector('.card__preview--number');
-    const cardHolderPreviewWrapper = document.querySelector('.card__preview--wrapper');
-    // const cardHolderPreview = document.querySelector('.card__holder--holder_value');
-    const cardHolderPreviewValue = document.querySelector('.card__holder--holder_value');
-    // const expirationDatePreviewLabel = document.querySelector('.card__preview--expiration');
-    const expirationDatePreviewValue = document.querySelector('.card__preview--expiration-date');
-    const cardMonthSelect = document.querySelector('#card__form--select_month');
-    const cardYearSelect = document.querySelector('#card__form--select_year');
-    const cardCVVInput = document.querySelector('.card__form--input_cvv');
-    const form = document.querySelector('.card__form');
+//I split validating and formating card number and cvv because number contains spaces
 
-    //card number
+const validateCardNumber = number => {
+    if (number.length >= 19) {
+        number = number.substring(0, 19);
+    }
+    const cleaned = number.replace(/\s+/g, '');
+    const matched = cleaned.match(/.{1,4}/g);
+    const joined = matched ? matched.join(' ') : '';
+
+    return joined;
+};
+
+const formatCardNumber = number => {
+    let cardNumberFormatted = '';
+        for (let i = 0; i < defaultCardNumber.length; i++) {
+            if (i < number.length) {
+                cardNumberFormatted += number[i];
+            } else if (defaultCardNumber[i] === ' ') {
+                cardNumberFormatted += ' '; // Keep spaces
+            } else {
+                cardNumberFormatted += '#'; // Mask remaining characters
+            }
+        }
+
+    return cardNumberFormatted;
+}
+
+const validateCVV = cvv => {
+    let cvvValue = cvv.replace(/\D/g, ''); 
+
+    if (cvvValue.length > 3) {
+        cvvValue = cvvValue.substring(0, 3);
+    };
+
+    return cvvValue;
+}
+
+const maskCVV = cvv => {
+    let maskedCVV = '';
+        for (let i = 0; i < 3; i++) {
+            if (i < cvv.length) {
+                maskedCVV += cvv[i]; 
+            } else {
+                maskedCVV += '#';
+            }
+        }
+
+    return maskedCVV;
+}
+
+export default function(){
+    const cardNumberInput = document.querySelector('.card_form--input_card_number');
+    const cardHolderInput = document.querySelector('.card_form--input_card_name');
+    const cardNumberPreview = document.querySelector('.card_preview--number');
+    const cardHolderPreviewWrapper = document.querySelector('.card_preview--wrapper');
+    const cardHolderPreviewValue = document.querySelector('.card__holder--holder_value');
+    const expirationDatePreviewValue = document.querySelector('.card_preview--expiration-date');
+    const cardMonthSelect = document.querySelector('#card_form--select_month');
+    const cardYearSelect = document.querySelector('#card_form--select_year');
+    const cardCVVInput = document.querySelector('.card_form--input_cvv');
+    const cardCVVPreview = document.querySelector('.card_preview--cvv');
+    const form = document.querySelector('.card_form');
 
     cardNumberInput.addEventListener('input', () => {
-        if (cardNumberInput.value.length >= 19) {
-            cardNumberInput.value = cardNumberInput.value.substring(0, 19);
-        }
+        const validatedInput = validateCardNumber(cardNumberInput.value);
+        // Update the input field (not preview) value with the formatted value
+        cardNumberInput.value = validatedInput;                    
 
-        //add space every four characters
-        const formatCardNumber = number => {
-            const cleaned = number.replace(/\s+/g, '');
-            const matched = cleaned.match(/.{1,4}/g);   
-            return matched ? matched.join(' ') : '';  
-        }
+        const formattedNumber = formatCardNumber(validatedInput);
 
-        cardNumberInput.addEventListener('input', () => {
-            const inputVal = cardNumberInput.value.replace(/\s/g, ''); // Remove spaces from the input
-            const formattedInput = formatCardNumber(inputVal);         // Format input with spaces
-            cardNumberInput.value = formattedInput;                    // Update the input field with the formatted value
-
-            // Prepare the card number preview: replace entered characters and mask the rest
-            let cardNumberFormatted = '';
-            for (let i = 0; i < defaultCardNumber.length; i++) {
-                if (i < formattedInput.length) {
-                    cardNumberFormatted += formattedInput[i];
-                } else if (defaultCardNumber[i] === ' ') {
-                    cardNumberFormatted += ' '; // Keep spaces
-                } else {
-                    cardNumberFormatted += '#'; // Mask remaining characters
-                }
-            }
-
-            cardNumberPreview.textContent = cardNumberFormatted;
-
-        })
-        
+        cardNumberPreview.textContent = formattedNumber;
     });
 
-    //card holder
 
     cardHolderInput.addEventListener('input', () => {
         cardHolderPreviewValue.textContent = cardHolderInput.value.toUpperCase() || defaultCardHolder;
     });
 
-    //expiration date
-
+    //one function to update value from both selects
     const updateExpirationDate = () => {
         //show only last two year digits
         expirationDatePreviewValue.textContent = `${cardMonthSelect.value || 'MM'}/${ cardYearSelect.value ? cardYearSelect.value.slice(2) : 'YY'}`;
@@ -68,35 +89,28 @@ export default function(){
     cardMonthSelect.addEventListener('change', updateExpirationDate);
     cardYearSelect.addEventListener('change', updateExpirationDate);
 
-    //cvv
-
-    const cvvPreview = document.createElement('p');
-    cvvPreview.classList.add('card__preview--cvv');
-    cvvPreview.textContent = 'CVV: ###';
-    cvvPreview.style.display = 'none';
-    document.querySelector('.card__preview').appendChild(cvvPreview);
-
     cardCVVInput.addEventListener('focus', () => {
         //hide other previews on focus and show cvv preview
         cardNumberPreview.style.display = 'none';
         cardHolderPreviewWrapper.style.display = 'none';
-        cvvPreview.style.display = 'block';
+        cardCVVPreview.style.display = 'block';
     });
 
     cardCVVInput.addEventListener('blur', () => {
         //show other previews and hide cvv preview
         cardNumberPreview.style.display = 'block';
         cardHolderPreviewWrapper.style.display = 'flex';
-        cvvPreview.style.display = 'none';
+        cardCVVPreview.style.display = 'none';
     });
 
     cardCVVInput.addEventListener('input', () => {
-        if (cardCVVInput.value.length >= 3) {
-            cardCVVInput.value = cardCVVInput.value.substring(0, 3);
-        }
+        const validatedInput = validateCVV(cardCVVInput.value);
 
-        const cvvValue = cardCVVInput.value || '###'; // Default value if empty
-        cvvPreview.textContent = `CVV: ${cvvValue}`;
+        cardCVVInput.value = validatedInput;
+
+        const maskedCVV = maskCVV(validatedInput);
+
+        cardCVVPreview.textContent = `CVV: ${maskedCVV}`;
     });
 
     form.addEventListener('submit', (e) => e.preventDefault()
